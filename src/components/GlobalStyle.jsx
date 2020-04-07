1322ef55bfa6d99d6de839e5b37a21d663ce668e
Joseph Rex <joerexko@gmail.com>
2020-04-07 13:34:33 -0500

    Fix button sibling placement

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -29,7 +29,7 @@ const GlobalStyle = createGlobalStyle`
   --drop-shadow-1: .1rem .1rem .2rem rgba(0, 0, 0, .05);
   --drop-shadow-2: 0 .1rem .2rem rgba(0, 0, 0, .3);
   --animation-easing: cubic-bezier(0.5, 0, 0.5, 1);
-  --site-width: 1300px;
+  --site-width: 1100px;
   font-size: 62.5%;
   scroll-behavior: smooth;
   line-height: 1.15;
