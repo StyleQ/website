39c88e1e30143baf5aed5a8c4080743316ef6c7a
Joseph Rex <joerexko@gmail.com>
2020-04-07 14:00:27 -0500

    Add section B

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Layout from '../templates/Layout';
 import Header from '../components/Header';
 import SectionA from '../components/SectionA';
+import SectionB from '../components/SectionB';
 
 function Home() {
   return (
     <Layout title="Home">
       <Header />
       <SectionA />
-      <h1>Container heading</h1>
+      <SectionB />
     </Layout>
   );
 }
