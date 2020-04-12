23273bb6d0b0c8830d20ac482f796b1adade01c0
Jessica Albert <jessmalbert0807@gmail.com>
2020-04-12 16:14:26 -0500

    Change App to be class
    Need to have a click handler function  for the hamburger menu.

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
 import ErrorBoundary from './components/ErrorBoundary';
 
-function App() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { navbarOpen: false };
+    this.handleNavbar = this.handleNavbar.bind(this);
+  }
 
-  return (
-    <ErrorBoundary>
-      <Router>
-        <Route exact path="/">
-          <Home
-            navbarState={navbarOpen}
-            handleNavbar={setNavbarOpen}
-          />
-        </Route>
-        <Route path="/book">
-          <Booking />
-        </Route>
-      </Router>
-    </ErrorBoundary>
-  );
+  handleNavbar() {
+    this.setState(state => ({
+      navbarOpen: !state.navbarOpen
+    }));
+  }
+
+  render() {
+    return (
+      <ErrorBoundary>
+        <Router>
+          <Route exact path="/">
+            <Home
+              navbarState={this.state.navbarOpen}
+              handleNavbar={this.handleNavbar}
+            />
+          </Route>
+          <Route path="/book">
+            <Booking />
+          </Route>
+        </Router>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
