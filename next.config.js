447d20a1c238148093eb3b46fb96f469649a2d01
Joseph Rex <joerexko@gmail.com>
2020-05-04 12:27:51 -0500

    Set up basic layout

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'react-svg-loader',
+          options: { jsx: true }
+        }
+      ],
+    });
+    return config;
+  },
+};
