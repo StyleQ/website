40bd99146f373c45a0859cd004744d06e5f81077
Ashlee <36325294+cannonashlee@users.noreply.github.com>
2020-05-16 19:16:13 -0700

    added button with chatbox link

--- components/SectionC.jsx
+++ components/SectionC.jsx
@@ -70,6 +70,7 @@ export default () => (
             Let StyleQ make booking your next service simple and stress-free.
           </Text>
         </Column>
+        <Button responsive variant="primary" as="a" href="https://app.engati.com/static/standalone/bot.html?bot_key=5e26745e9b794e60">Start your survey here</Button>
       </Grid>
     </Container>
   </Root>
